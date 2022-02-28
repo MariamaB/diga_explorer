e3f8b5939faf3fe1f53e0f1280193bf7b5dad364
MariamaB <s0539148@htw-berlin.de>
2022-02-28 16:13:32 +0100

    remove not existing import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:diga_explorer/common/AuthenticationWrapper.dart';
-import 'package:diga_explorer/models/dashboard_listener.dart';
 import 'package:diga_explorer/services/auth_service.dart';
 import 'package:diga_explorer/services/firestore_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
